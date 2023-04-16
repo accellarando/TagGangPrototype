98d340ed54722aea2abff325a7778b620e5eeeb9
Ella Moss <ella.moss@utah.edu>
2023-04-15 18:07:59 -0600

    testin clutter

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+CFLAGS_GFREENECT := -I/usr/local/include/gfreenect-0.1
+LDFLAGS_GFREENECT := -lgfreenect-0.1
+
 all:
-	gcc main.c `pkg-config --cflags --libs glib-2.0 clutter-1.0` -o main.out
+	gcc main.c `pkg-config --cflags --libs glib-2.0 clutter-1.0` -o main.out $(CFLAGS_GFREENECT) $(LDFLAGS_GFREENECT)
 
 clean:
 	rm -f main.out
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * This is some example source code to do some basic skeleton tracking to control a stepper motor.
  *
- * Much of this source code is taken from Skeltrack's test-kinect example.
+ * Much of this source code is taken from Skeltrack's test-kinect example, and joaquimrocha's Skeltrack-Desktop-Control code.
  *
  * @author Ella Moss
  */
@@ -34,104 +34,72 @@ static guint THRESHOLD_END   = 1500;
 
 typedef struct
 {
-  guint16 *reduced_buffer;
-  gint width;
-  gint height;
-  gint reduced_width;
-  gint reduced_height;
+	guint16 *reduced_buffer;
+	gint width;
+	gint height;
+	gint reduced_width;
+	gint reduced_height;
 } BufferInfo;
 
-static void on_new_kinect_device (GObject      *obj,
-                      GAsyncResult *res,
-                      gpointer      user_data)
+static void set_info_text (void)
 {
-  ClutterActor *stage, *instructions;
-  GError *error = NULL;
-  gint width = 640;
-  gint height = 480;
+  gchar *title;
+  title = g_strdup_printf ("<b>Current View:</b> %s\n"
+                           "<b>Threshold:</b> %d",
+                           SHOW_SKELETON ? "Skeleton" : "Point Cloud",
+                           THRESHOLD_END);
+  clutter_text_set_markup (CLUTTER_TEXT (info_text), title);
+  g_free (title);
+}
 
-  kinect = gfreenect_device_new_finish (res, &error);
-  if (kinect == NULL)
-    {
-      g_debug ("Failed to created kinect device: %s", error->message);
-      g_error_free (error);
-      clutter_main_quit ();
-      return;
-    }
+static void on_new_kinect_device (GObject *obj, GAsyncResult *res, gpointer user_data){
+	ClutterActor *stage, *instructions;
+	GError *error = NULL;
+	gint width = 640;
+	gint height = 480;
 
-  g_debug ("Kinect device created!");
+	kinect = gfreenect_device_new_finish (res, &error);
+	if (kinect == NULL)
+	{
+		/*
+		printf("Kinect was null: %s\n",error->message);
+		g_error_free (error);
+		clutter_main_quit ();
+		return;
+		*/
+	}
 
-  stage = clutter_stage_new ();
-  clutter_stage_set_title (CLUTTER_STAGE (stage), "Kinect Test");
-  clutter_actor_set_size (stage, width * 2, height + 250);
-  clutter_stage_set_user_resizable (CLUTTER_STAGE (stage), TRUE);
+	printf("Kinect created!\n");
 
-  g_signal_connect (stage, "destroy", G_CALLBACK (on_destroy), kinect);
-  g_signal_connect (stage,
-                    "key-release-event",
-                    G_CALLBACK (on_key_release),
-                    kinect);
 
-  depth_tex = clutter_actor_new ();
-  depth_canvas = clutter_canvas_new ();
-  clutter_actor_set_content (depth_tex, depth_canvas);
-  clutter_canvas_set_size (CLUTTER_CANVAS (depth_canvas), width, height);
-  clutter_actor_set_size (depth_tex, width, height);
-  clutter_actor_add_child (stage, depth_tex);
-
-  video_tex = clutter_actor_new ();
-  clutter_actor_set_content (video_tex, clutter_image_new ());
-  clutter_actor_set_size (video_tex, width, height);
-  clutter_actor_set_position (video_tex, width, 0.0);
-  clutter_actor_add_child (stage, video_tex);
+	stage = clutter_stage_get_default ();
+	clutter_stage_set_title (CLUTTER_STAGE (stage), "TagGang Prototype");
+	clutter_actor_set_size (stage, width, height + 220);
+	clutter_stage_set_user_resizable (CLUTTER_STAGE (stage), TRUE);
 
   info_text = clutter_text_new ();
-  clutter_actor_set_position (info_text, 50, height + 20);
-  clutter_actor_add_child (stage, info_text);
-
-  instructions = create_instructions ();
-  clutter_actor_set_position (instructions, 50, height + 70);
-  clutter_actor_add_child (stage, instructions);
-
-  skeleton = skeltrack_skeleton_new ();
-  g_object_get (skeleton, "smoothing-factor", &SMOOTHING_FACTOR, NULL);
-
   set_info_text ();
+  clutter_actor_set_position (info_text, 50, height + 20);
+  clutter_container_add_actor (CLUTTER_CONTAINER (stage), info_text);
 
-  g_signal_connect (kinect,
-                    "depth-frame",
-                    G_CALLBACK (on_depth_frame),
-                    NULL);
-
-  g_signal_connect (kinect,
-                    "video-frame",
-                    G_CALLBACK (on_video_frame),
-                    NULL);
-
-  g_signal_connect (depth_canvas,
-                    "draw",
-                    G_CALLBACK (on_skeleton_draw),
-                    NULL);
-
-  gfreenect_device_set_tilt_angle (kinect, 0, NULL, NULL, NULL);
-
-  gfreenect_device_start_depth_stream (kinect,
-                                       GFREENECT_DEPTH_FORMAT_MM,
-                                       NULL);
-
-  gfreenect_device_start_video_stream (kinect,
-                                       GFREENECT_RESOLUTION_MEDIUM,
-                                       GFREENECT_VIDEO_FORMAT_RGB, NULL);
-
-  clutter_actor_show (stage);
+	clutter_actor_show_all(stage);
 }
+
 int main(int argc, char* argv[]){
 	printf("Hello world!\n");
 
+
+	if (clutter_init (&argc, &argv) != CLUTTER_INIT_SUCCESS)
+    {
+      return -1;
+    }
 	gfreenect_device_new(0,
 			GFREENECT_SUBDEVICE_CAMERA,
 			NULL,
 			on_new_kinect_device,
 			NULL);
+
+	clutter_main();
+
 	return 0;
 }
