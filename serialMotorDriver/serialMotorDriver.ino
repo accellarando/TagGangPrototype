f2476f582b6289d90cb10ecb227496f32b40b53d
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-04-17 17:55:19 -0600

    created .ino file for serial comms between py and arudino and motor movement from driver

---
+++ serialMotorDriver/serialMotorDriver.ino
@@ -0,0 +1,19 @@
+// serial comm between python and arduino
+  // https://www.hackster.io/ansh2919/serial-communication-between-python-and-arduino-e7cce0
+
+// https://github.com/accellarando/TagGang/blob/main/docs/meetings/20230415.txt
+  // Computer translates that information to a stepper motor command, e.g. "-3" or "24"
+  // .py script to send the stepper motor command to the arduino over serial -> ./stepper_control.py -3
+  // ! Arduino picks up this serial command
+  // ! Arduino sends serial command to stepper driver
+  // ! Stepper motor moves to desired position
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
