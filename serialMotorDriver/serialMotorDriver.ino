bd9532f237ac092f25c95ac1d685bb70309cb44d
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-04-17 17:56:27 -0600

    changed comment

--- serialMotorDriver/serialMotorDriver.ino
+++ serialMotorDriver/serialMotorDriver.ino
@@ -3,7 +3,7 @@
 
 // https://github.com/accellarando/TagGang/blob/main/docs/meetings/20230415.txt
   // Computer translates that information to a stepper motor command, e.g. "-3" or "24"
-  // .py script to send the stepper motor command to the arduino over serial -> ./stepper_control.py -3
+  // .py script to send the stepper motor command to the arduino over serial -> e.g. (stepper control)./move_stepper.py -3
   // ! Arduino picks up this serial command
   // ! Arduino sends serial command to stepper driver
   // ! Stepper motor moves to desired position
