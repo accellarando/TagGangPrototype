362f8dbd81a2fb55e2e05b8a8fc7f20382881a91
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-04-17 17:38:49 -0600

    links in py file

--- move_stepper.py
+++ move_stepper.py
@@ -4,6 +4,9 @@
 # https://github.com/accellarando/TagGang/blob/main/docs/meetings/20230415.txt
 # https://github.com/accellarando/TagGangPrototype/blob/main/shellOutput.txt\
 # https://github.com/accellarando/TagGangPrototype/blob/main/main2.c
+# https://www.youtube.com/watch?v=7spK_BkMJys
+# https://howtomechatronics.com/tutorials/arduino/stepper-motors-and-arduino-the-ultimate-guide/
+# https://docs.google.com/document/d/15zFXHQggSIYDtgaQV2rhGvOE4UlRhYPvJxaJMdB6EUI/edit?usp=sharing
 
 import os #and whatever other libraries you'll need
 
