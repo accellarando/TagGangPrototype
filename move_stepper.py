98d0d748c04e012c22204010f75ffa565280f037
Ella Moss <ella.moss@utah.edu>
2023-04-17 17:29:49 -0600

    python skeleton

---
+++ move_stepper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+#Python pseudocode for moving the stepper motor
+
+import os #and whatever other libraries you'll need
+
+#This function should read the arg from the command line and return a number
+def get_cmd_arg():
+    return 0
+
+#This function should connect to arduino and send the number of steps we want to move
+def send_to_arduino(steps):
+
+
+if __name__ == "__main__":
+    steps = get_cmd_arg()
+    send_to_arduino(steps)
