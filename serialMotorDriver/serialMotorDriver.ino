5d91787142f197e8a0e8a4e7053faabd6c5ea23d
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-04-20 00:56:40 -0600

    think basic motor movement based on incoming data is done, need to fix and debug tho

--- serialMotorDriver/serialMotorDriver.ino
+++ serialMotorDriver/serialMotorDriver.ino
@@ -24,40 +24,52 @@ void loop() {
 
 #include <AccelStepper.h>
 
-// Define the pins connected to the stepper motor driver
+// Define the stepper motor connections
+#define STEPS_PER_REV 200 // Number of steps per revolution for the stepper motor
 #define STEP 5 // Connect step pin to A5
 #define DIR 4 // Connect direction pin to A4
 //int STEP = 5
 //pinMode(STEP, OUTPUT);
 // ?? Microstepping pins
 
-#define STEPS_PER_REV 200 // Define the number of steps per revolution for the stepper motor
 // !! Math already done in move_stepper() main2.c, no need to modify position values in define
 
 // Private variables
 int steps;
+String input;
+
 AccelStepper stepper(AccelStepper::DRIVER, STEP, DIR); // Instance of the AccelStepper class for the stepper motor
 
 void setup() {
   // Configure the serial connection
   // !! https://www.google.com/search?q=how+to+figure+out+what+baud+rate+to+use+arduino&rlz=1C1CHBF_enUS773US773&oq=how+to+figureout+what+baud+rate+to+use+ar&aqs=chrome.1.69i57j33i10i160l3.10089j0j7&sourceid=chrome&ie=UTF-8
-  Serial.begin(115200); // !! Set baud rate for serial port FIXME
+  Serial.begin(115200); // !! Set baud rate for serial port FIXME DO WE NEED?
   //Serial.setTimeout(1); // Set max of 1 ms wait for serial data, doc: default 1000 ms 
 
   // Configure the stepper motor
   stepper.setMaxSpeed(1000); // Set max speed value for the stepper (steps/sec), doc: <1000 steps unreliable
-  stepper.setAcceleration(1000); // Set acceleration (steps per second per second), doc: >0.0 acc
+  stepper.setAcceleration(500); // Set acceleration (steps per second per second), doc: >0.0 acc
 }
 
+/**
+ * Serial communication between serialMotorCMD.py script
+ */
 void loop() {
-  // Serial communication between serialMotorCMD.py script 
-  //int stepVal = 0; !! put outside
+  // Check if there is data available to read from the serial port
   if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n'); // Read incoming data from serialMotorDriver.py as a string (see subprocess1.txt for expl.)
+    input = Serial.readStringUntil('\n'); // Read incoming data from serialMotorDriver.py as a string (see subprocess1.txt for expl.)
+    steps = input.toInt(); // Convert the data string to an integer
     //stepVal = Serial.parseInt();
-    steps = input.toInt(); // Convert the string to an integer
 
-    // DO something with step value
+    // Move the stepper motor by the incoming-data-specified number of steps (non-blocking)
+    stepper.move(steps); // move() accounts for positions in both directions relative to current position
+    //stepper.moveTo(steps); // long absolute
+
+    // Wait until the stepper motor has reached its target position
+    // distanceToGo() is distance from the current position to the target position in steps in both directions
+    while (stepper.distanceToGo() ! = 0){
+      stepper.run();
+    }
   }
 
   // !! SETUP: restart to default position
