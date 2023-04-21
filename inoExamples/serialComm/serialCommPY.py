0b23f3da6045852c493ba0852e2430623dd24a9c
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-04-20 21:02:42 -0600

    example not sending right value back to python output?

--- inoExamples/serialComm/serialCommPY.py
+++ inoExamples/serialComm/serialCommPY.py
@@ -2,27 +2,45 @@
     # https://www.hackster.io/ansh2919/serial-communication-between-python-and-arduino-e7cce0
 
 import serial
-
-def send_receive_from_arduino(x):
-    # Establish connection to Arduino Uno
-    ser = serial.Serial('COM9', 9600)
-    # Encode and send data to Arduino
-    ser.write(str(x).encode())
-    # Wait for Arduino to process data and send response
-    arduino_data = ser.readline().decode().strip()
-    # Close connection to Arduino
-    #ser.close()
-    # Return response from Arduino
-    return arduino_data
+import time
 
 def increment_input():
-    # Ask user for input and convert to integer
-    num = int(input("Enter a number: "))
-    #print(num)
-    # Add 1 to input using Arduino
-    incre = (int(send_receive_from_arduino(num)))
-    # Print the incremented number
-    print("Incremented number: ", incre)
+    while True:
+        inputNum = int(input("Enter a number: "))
+
+        #print(inputNum)
+        #print(str(inputNum).encode()) # prints b'3' (bytes object containing the UTF-8 encoded string "3")
+
+        incrementNum = (int(send_receive_arduino(inputNum)))
+        
+        print("Incremented number: ", incrementNum)
+
+# args = int
+def send_receive_arduino(x):
+    ser = serial.Serial('COM9', 9600)
+    #time.sleep(1.5)
+    # x data is of int type, must convert int to string then to bytes before sending
+    # with encode() as this sends as a byte string == byte stream
+    ser.write(str(x).encode())
+    
+    # wait for modified data from arduino to be sent back
+    # decode() only if reading data that is sent from the Arduino to the Python code
+    # (if the data is sent in a non-ASCII format, such as bytes or a string of bytes)
+    # to be interpreted correctly in python
+
+    response = ser.read().decode().strip()
+    #.decode().strip()
+
+    # do not need a Serial .read..() function if the arduino prints the final output
+    # only need it if you want to capture or process the output from the Arduino 
+    # in your Python script
+
+    
+
+    # must close port when done using, then return response data from arduino if .read()
+    ser.close()
+    return int(response)
+
 
 # Call the increment_input function to start the program
 increment_input()
