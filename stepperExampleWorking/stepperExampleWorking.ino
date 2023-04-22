ea9f2327131dc8364e2871b2eabf0cc325205a55
Ella Moss <ella.moss@utah.edu>
2023-04-21 19:07:33 -0600

    ok now it works

--- stepperExampleWorking/stepperExampleWorking.ino
+++ stepperExampleWorking/stepperExampleWorking.ino
@@ -1,6 +1,3 @@
-#include <AccelStepper.h>
-#include <MultiStepper.h>
-
 #define STEP_PIN 6
 #define DIR_PIN 7
 #define ENABLE_PIN 2
@@ -9,9 +6,15 @@
 #define VREFA_PIN 3
 #define VREFB_PIN 9
 
-AccelStepper stepper(AccelStepper::FULL2WIRE, STEP_PIN, DIR_PIN);
+/*   
+ *   Basic example code for controlling a stepper without library
+ *      
+ *   by Dejan, https://howtomechatronics.com
+ */
 
-void setup(){
+void setup() {
+  // Sets the two pins as Outputs
+
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
@@ -21,11 +24,25 @@ void setup(){
   analogWrite(VREFA_PIN, 40);
   analogWrite(VREFB_PIN, 40);
   digitalWrite(ENABLE_PIN, HIGH);
-  
 }
-
-void loop(){
-  stepper.moveTo(-stepper.currentPosition());
-  stepper.run();
-  delay(500);
+void loop() {
+  digitalWrite(DIR_PIN,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 800; x++) {
+    digitalWrite(STEP_PIN,HIGH); 
+    delayMicroseconds(700);    // by changing this time delay between the steps we can change the rotation speed
+    digitalWrite(STEP_PIN,LOW); 
+    delayMicroseconds(700); 
+  }
+  delay(1000); // One second delay
+  
+  digitalWrite(DIR_PIN,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+  for(int x = 0; x < 1600; x++) {
+    digitalWrite(STEP_PIN,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(STEP_PIN,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
 }
